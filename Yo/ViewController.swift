f8b1259388c1afd68566c034bc51b64dc6785b77
tomiant1 <tomi@joinhangoo.com>
2022-12-10 11:20:09 +0100

    Initial Commit

---
+++ Yo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yo
+//
+//  Created by Tomi Antoljak on 12/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
