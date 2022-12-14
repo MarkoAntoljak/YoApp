e6eedda807ceb5e5ea6ada828296808c89d0d83e
MarkoAntoljak <marko@joinhangoo.com>
2022-12-14 18:23:02 +0100

    Revert "Merge branch 'main'"
    This reverts commit ee72a231bebbf7831afc4db86d2e8763993c0da8, reversing
    changes made to f7a60574881c2d9acc7e2e400007c20c9dc8d5b3.

---
+++ Yo/Controllers/Main/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Yo
+//
+//  Created by Tomi Antoljak on 12/10/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+    }
+
+}
